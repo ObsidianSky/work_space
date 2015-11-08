cf0b22dff5e9195426675e0ecfdcfc68de3c76a0
Vlad Nadtoka <vlad.nadtoka93@gmail.com>
2015-11-08 22:51:37 +0200

    minify css, smart image min and fonticon generator added

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,11 @@ var gulp = require('gulp'),
     pngquant = require('imagemin-pngquant'),
     rimraf = require('rimraf'),
     rigger = require('gulp-rigger'),
-    // plumber = require('gulp-plumber'),
-    connect = require('gulp-connect');
+    changed = require('gulp-changed'),
+	cssmin = require('gulp-cssmin'),
+    connect = require('gulp-connect'),
+	iconfont = require('gulp-iconfont'),
+	iconfontCss = require('gulp-iconfont-css');
 
 var onError = function (err){  
   console.log(err);
@@ -23,14 +26,16 @@ var path = {
         js: 'html/js',
         img: 'html/img/',
         libs: 'html/libs',
-        fonts: 'html/fonts/'
+        fonts: 'html/fonts/',
+        iconfont: 'html/fonts/iconfont/'
     },
     dev: { 
         html: 'dev/*.*', 
         css: 'dev/css/**/*.css',
         less: 'dev/less/all.less',
         js: 'dev/js/**/*.js',
-        img: 'dev/img/**/*.*', 
+        img: 'dev/img/**/*.*',
+        iconfont: 'dev/img/iconfont/*.svg',
         sprites: 'dev/img/sprites/*.*',
         libs: 'dev/libs/**/*.js',
         fonts: 'dev/fonts/**/*.*'
@@ -42,6 +47,7 @@ var path = {
         less: 'dev/less/**/*.less',
         js: 'dev/js/**/*.js',
         img: 'dev/img/**/*.*',
+        iconfont: 'dev/img/iconfont/*.svg',
         sprite: 'dev/img/sprites/*.*',
         libs: 'dev/libs/**/*.*',
         fonts: 'dev/fonts/**/*.*'
@@ -105,23 +111,26 @@ gulp.task('less:build', function () {
 
 gulp.task('css:build', function () {
     gulp.src(path.dev.css) 
+    .pipe(changed(path.build.css))
+    .pipe(cssmin())
     .pipe(gulp.dest(path.build.css))
     .pipe(connect.reload());
 });
 
 gulp.task('image:build', function () {
-    gulp.src(path.dev.img)
+    gulp.src([path.dev.img, '!dev/img/sprites/*.*', '!dev/img/iconfont/*.*'])
+    .pipe(changed(path.build.img))
+    .pipe(imagemin({
+        progressive: true,
+        use: [pngquant()],
+        interlaced: true
+    }))
     .pipe(gulp.dest(path.build.img))
     .pipe(connect.reload());
 });
 
 gulp.task('image:optimize', function(){
     gulp.src(path.dev.img) 
-    .pipe(imagemin({
-        progressive: true,
-        use: [pngquant()],
-        interlaced: true
-    }))
     .pipe(gulp.dest(path.build.img))
     .pipe(connect.reload());
 });
@@ -132,6 +141,23 @@ gulp.task('fonts:build', function() {
     .pipe(connect.reload());
 });
 
+gulp.task('iconfont:build', function(){
+  gulp.src([path.dev.iconfont], {base:'dev/'})
+    .pipe(iconfontCss({
+      fontName: 'icons',
+      path: 'css',
+      targetPath: '../../../dev/less/fonticons.less',
+      fontPath: '../fonts/iconfont/',
+      cssClass:'fico'
+    }))
+    .pipe(iconfont({
+      fontName: 'icons',
+      formats: ['ttf', 'eot', 'woff', 'svg'],
+      appendUnicode: true
+     }))
+    .pipe(gulp.dest(path.build.iconfont));
+});
+
 gulp.task('build', [
     'html:build',
     'libs:build',
@@ -140,7 +166,8 @@ gulp.task('build', [
     'css:build',
     'less:build',
     'image:build',
-    'fonts:build'
+    'fonts:build',
+    'iconfont:build'
 ]);
 
 
@@ -154,28 +181,41 @@ gulp.task('watch', function(){
     watch([path.watch.templates], function(event, cb) {
         gulp.start('html:build');
     }); 
+
     watch([path.watch.html], function(event, cb) {
         gulp.start('html:build');
     }); 
+
     watch([path.watch.css], function(event, cb) {
         gulp.start('css:build');
     });
+
     watch([path.watch.less], function(event, cb) {
         gulp.start('less:build');
     });
+
     watch([path.watch.js], function(event, cb) {
         gulp.start('js:build');
     });
+
     watch([path.watch.libs], function(event, cb) {
         gulp.start('libs:build');
     });
-    watch([path.watch.img], function(event, cb) {
+
+    watch([path.watch.img,'!dev/img/sprites/*.*','!dev/img/iconfont/*.*'], function(event, cb) {
         gulp.start('image:build');
     });
+
+    watch([path.watch.iconfont], function(event, cb) {
+        gulp.start('iconfont:build');
+    });
+
     watch([path.watch.sprite], function(event, cb) {
         gulp.start('sprite:build');
     });
+
     watch([path.watch.fonts], function(event, cb) {
         gulp.start('fonts:build');
     });
+
 });
